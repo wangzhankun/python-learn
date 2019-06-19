24b336cf058e6cab6847b226b1f5fc406fa85ada
wangzhankun <bitwangzhankun@gmail.com>
2019-06-19 16:29:47 +0800

    update

--- word2vec_skipgram/skipgram.py
+++ word2vec_skipgram/skipgram.py
@@ -11,7 +11,9 @@ with open("word2vec_skipgram\\text8") as f:
     text = f.read()
 print(type(text))
 print(len(text))
-text = [word for word in text.split()]
+#%%
+text = list(text)
+#text = [word for word in text.split()]
 #%%
 print(text[:100])
 print(len(text))
@@ -19,10 +21,14 @@ print(len(text))
 # preprocess
 tokenizer = WordPunctTokenizer()
 words_count = Counter(text)
-#删除单词数目少于5的噪声
-words = [tokenizer.tokenize(word.lower())  for word in text if words_count[word] > 5]
+#%%
+#删除单词数目少于5的噪声     
+words = [tokenizer.tokenize(word.lower())  for word in text[:100] if words_count[word] > 5]
 #%%
 print(words[:50])
-print(type(words))
+print(type(words[1]))
 print("Total words: {}".format(len(words)))
 
+
+
+#%%
